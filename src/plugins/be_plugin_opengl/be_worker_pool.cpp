11dc31820e1faf5a0d3dc84845be201b5e9b7e6e
bobke <bob.winckelmans@telenet.be>
2023-04-01 01:52:21 +0200

    remove comment

--- src/plugins/be_plugin_opengl/be_worker_pool.cpp
+++ src/plugins/be_plugin_opengl/be_worker_pool.cpp
@@ -4,25 +4,6 @@
 	{
 		if(workerCount>0)
 		{
-			m_pool=boost::shared_ptr<boost::threadpool::pool>( new boost::threadpool::pool(workerCount) );
+			m_pool = boost::shared_ptr<boost::threadpool::pool>( new boost::threadpool::pool(workerCount) );
 		}
 	}
-
-// 	template<typename T>
-// 	boost::shared_future<T> schedule(boost::function<T (void)> const& fn)
-// 	{
-// 		if(m_pool)
-// 		{
-// 			boost::shared_ptr<boost::packaged_task<T> > task(new boost::packaged_task<T>(fn));
-// 			boost::shared_future<T> res(task->get_future());
-// 			m_pool->schedule(boost::bind(&boost::packaged_task<T>::operator(), task));
-// 			return res;
-// 		}
-// 		else
-// 		{
-// 			boost::promise<T> promise;
-// 			promise.set_value(fn());
-// 			return promise.get_future();
-// 		}
-// 	}
-	
